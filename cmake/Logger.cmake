4fe257520f884b7285ba19a1945c313eeaebd0b3
Vito Gamberini <vito@gamberini.email>
2021-05-03 02:05:03 -0400

    Fix Github Actions build failures
    Ubuntu Focal's version of Boost is older than our build requirements,
    this compensates until Actions provides an updated VM

--- cmake/Logger.cmake
+++ cmake/Logger.cmake
@@ -8,7 +8,7 @@ swig_add_library(Logger
 set_target_properties(Logger PROPERTIES OUTPUT_NAME CLogger
     SWIG_USE_TARGET_INCLUDE_DIRECTORIES TRUE SWIG_COMPILE_OPTIONS -py3
     LIBRARY_OUTPUT_DIRECTORY ${RKR_PACKAGE_ROOT})
-target_compile_features(Logger PRIVATE cxx_std_20)
+target_compile_features(Logger PRIVATE cxx_std_17)
 target_compile_options(Logger PRIVATE ${OPTIONS})
 target_include_directories(Logger PRIVATE ${INCLUDES})
 target_link_libraries(Logger ${Boost_LOG_LIBRARY})
